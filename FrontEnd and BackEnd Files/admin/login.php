3e6c9fa822631793ad4d5353e799bb978b820365
RhythmSingla <56694788+RhythmSingla@users.noreply.github.com>
2020-12-07 11:16:51 +0530

    Add files via upload

---
+++ FrontEnd and BackEnd Files/admin/login.php
@@ -0,0 +1,48 @@
+<?php 
+session_start(); 
+include "db_conn.php";
+
+if (isset($_POST['uname']) && isset($_POST['password'])) {
+
+	function validate($data){
+       $data = trim($data);
+	   $data = stripslashes($data);
+	   $data = htmlspecialchars($data);
+	   return $data;
+	}
+
+	$uname = validate($_POST['uname']);
+	$pass = validate($_POST['password']);
+
+	if (empty($uname)) {
+		header("Location: index.php?error=User Name is required");
+	    exit();
+	}else if(empty($pass)){
+        header("Location: index.php?error=Password is required");
+	    exit();
+	}else{
+		$sql = "SELECT * FROM records_admin WHERE User_Name='$uname' AND User_Pwd='$pass'";
+
+		$result = mysqli_query($conn, $sql);
+
+		if (mysqli_num_rows($result) === 1) {
+			$row = mysqli_fetch_assoc($result);
+            if ($row['User_Name'] === $uname && $row['User_Pwd'] === $pass) {
+            	$_SESSION['User_ID'] = $row['User_ID'];
+            	$_SESSION['User_Email'] = $row['User_Email'];
+            	header("Location: http://localhost/CRUD/pages/forms/Admin%20Dashboard.html");
+		        exit();
+            }else{
+				header("Location: index.php?error=Incorrect User name or password");
+		        exit();
+			}
+		}else{
+			header("Location: index.php?error=Incorrect User name or password");
+	        exit();
+		}
+	}
+	
+}else{
+	header("Location: index.php");
+	exit();
+}
---
+++ FrontEnd and BackEnd Files/dist/Readme by Rhythm.txt
@@ -0,0 +1,3 @@
+This folder contains the main css folder for the dashboard.
+Images folder for all the images on the dashboard.
+JS folder involving js codes for animation of side-menu push button, etc.
