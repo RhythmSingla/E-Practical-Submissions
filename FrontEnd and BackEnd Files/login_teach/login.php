2f93e00d33b8de07a8c6dc996857487534c0c783
RhythmSingla <56694788+RhythmSingla@users.noreply.github.com>
2020-12-07 11:20:37 +0530

    Add files via upload

---
+++ FrontEnd and BackEnd Files/login_teach/login.php
@@ -0,0 +1,49 @@
+<?php 
+session_start(); 
+include "db_conn.php";
+
+if (isset($_POST['uname']) && isset($_POST['password'])) {
+
+	function validate($data){
+       $data = trim($data);
+	   $data = stripslashes($data);
+	   $data = htmlspecialchars($data);
+	   return $data;
+	}
+
+	$uname = validate($_POST['uname']);
+	$pass = validate($_POST['password']);
+
+	if (empty($uname)) {
+		header("Location: index.php?error=User Name is required");
+	    exit();
+	}else if(empty($pass)){
+        header("Location: index.php?error=Password is required");
+	    exit();
+	}else{
+		$sql = "SELECT * FROM records_teach WHERE User_Name='$uname' AND User_Pwd='$pass'";
+
+		$result = mysqli_query($conn, $sql);
+
+		if (mysqli_num_rows($result) === 1) {
+			$row = mysqli_fetch_assoc($result);
+            if ($row['User_Name'] === $uname && $row['User_Pwd'] === $pass) {
+            	$_SESSION['User_ID'] = $row['User_ID'];
+            	$_SESSION['User_Email'] = $row['User_Email'];
+            	$_SESSION['User_Roll'] = $row['User_Roll'];
+            	header("Location: http://localhost/CRUD/pages/forms/Teacher%20Dashboard.html");
+		        exit();
+            }else{
+				header("Location: index.php?error=Incorrect User name or password");
+		        exit();
+			}
+		}else{
+			header("Location: index.php?error=Incorrect User name or password");
+	        exit();
+		}
+	}
+	
+}else{
+	header("Location: index.php");
+	exit();
+}
